c6787c8c2466f100437e97352eb7f2076d656699
Klatka <90255169+Hideusz@users.noreply.github.com>
2022-02-15 15:20:58 +0100

    Create zadanie.cpp

---
+++ zadanie.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a,c,d;
+    int b;
+    cout << "Podaj swoje imie i nazwisko" << endl;
+    cin >> a;
+    cout << "Podaj swoj wiek" << endl;
+    cin >> b;
+    cout << "Podaj swoj adres" << endl;
+    cin >> c;
+    cout << "Jaki masz dzisiaj humor" << endl;
+    cin >> d;
+    cout << "Dziekuje za informacje " << endl;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+
+}
